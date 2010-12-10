99817018d9c8658de0de2d9097e1d175c8b55849
Robin Beitra <robin@beitra.net>
2010-12-10 15:00:25 +0000

    make smallerererer

--- src/joop.js
+++ src/joop.js
@@ -21,25 +21,29 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-var Joop = Joop || function (classname, definition) {
-    function c(source, target, preserve) {//copy values from one object to another
+var Joop = Joop || (function (scope, syntax) {
+    
+    function copy(source, target, preserve) {//copy values from one object to another
         for (var name in source) {
             if (source.hasOwnProperty(name) && !(preserve && target[name])) {
                 target[name] = source[name];
             }
         }
     }
-    function e(target, name, value) {//ensure target has value, or use default
+    
+    function ensure(target, name, value) {//ensure target has value, or use default
         return (target[name] = target[name] || value);
     }
-    function f(o) {//a function maker
+    
+    function funner(proto) {//a function maker
         function G() {}
-        if (o) {
-            G.prototype = o;
+        if (proto) {
+            G.prototype = proto;
         }
         return G;
     }
-    function g(target, path, create, limit) {//get a reference by path. optionally create missing nodes
+    
+    function getter(target, path, create, limit) {//get a reference by path. optionally create missing nodes
         var parts = path.split('.'),
             max = parts.length,
             i, part;
@@ -47,81 +51,78 @@ var Joop = Joop || function (classname, definition) {
         for (i = 0; i < limit; i += 1) {
             part = parts[i];
             if (create) {
-                e(target, part, {});
+                ensure(target, part, {});
             }
             target = target[part];
         }
         return target;
     }
-    if (!definition) {
-        definition = classname;
-        classname = '';
-    }
-    var t = Joop, k = t.k, a, b, i, j,//some local variables
-    //make some shorter vars for the syntax
-        kx = k.x,//extend/inherit
-        km = k.m,//members
-        ks = k.s,//statics
-        kf = k.f,//non-constructor
-        kn = k.n,//classname
-        kc = k.c,//constructor
-        G = f(e(definition, k.p, {})),//ensure we have a prototype
-        p = new G();
        
-    function l(value, target, path) {//link value at path in target.
+    function link(value, target, path) {//link value at path in target.
         var endPart;
-        path = path || value[kn];
+        path = path || value[syntax.n];
         endPart = path.split('.').pop();
-        target = g(target, path, true, -1);//now get the parent, which could == target if parts.length == 1
-        c(e(target, endPart, {}), value, 1);//preserve values from the new guy
+        target = getter(target, path, true, -1);//now get the parent, which could == target if parts.length == 1
+        copy(ensure(target, endPart, {}), value, 1);//preserve values from the new guy
         target[endPart] = value;
-    } 
-    
-    //inherit from any base classes
-    a = e(definition, kx, []);
-    for (i = 0; i < a.length; i += 1) {
-        j = a[i];
-        j = (typeof j === 'string') ? j : j[kn];//we accept Joop constructors and strings.
-        b = g(t.$, j, false, 0);//find the constructor in our scope
-        c(b.prototype, p);
-        //copy(b.prototype, p, j + '.');//this is a bit pointless and makes huge messy classes
-        p[j] = b;//give a reference to the inherited constructor
-    } 
-    
-    //add any members
-    c(e(definition, km, {}), p);
-            
-    //check if we have this constructor, if so run as constructor otherwise run as non-constructor!
-    a = function () {
-        b = arguments;
-        if (this[this[kn]]) {
-            return a[kc].apply(this, b);
-        }
-        return a[kf].apply(a, b);
-    };
-    
-    //set up prototype
-    p[classname] = a;
-    p[kn] = classname;
-    
-    //set up class info etc
-    a.prototype = p;//use proper keyword here
-    a[k.d] = definition;//
-    a[kc] = e(definition, kc, function () {//default constructor will take an init object and copy its contents
-        c(arguments[0] || {}, this);
-    });
-    a[kf] = e(definition, kf, f());//copy in non-constructor
-    c(e(definition, ks, {}), a);//copy in statics
-//        a[x] = definition[x];//copy in list of bases
-    a[kn] = classname;//set classname
-    
-    if (classname) {
-        l(a, t.$);//a has a classname defined so t.l will use that
     }
-    return a;
-};
-Joop.$ = this;//set up default scope
-Joop.k = {//keywords are editable if you don't like them
+    
+    function define(classname, definition) {//define a class and link it if a classname is supplied
+        if (!definition) {
+            definition = classname;
+            classname = '';
+        }
+        
+        var a, b, i, j,//some local variables
+            G = funner(ensure(definition, syntax.p, {})),//ensure we have a prototype
+            proto = new G();
+        
+        //inherit from any base classes
+        a = ensure(definition, syntax.x, []);
+        for (i = 0; i < a.length; i += 1) {
+            j = a[i];
+            j = (typeof j === 'string') ? j : j[syntax.n];//we accept Joop constructors and strings.
+            b = getter(scope, j, false, 0);//find the constructor in our scope
+            copy(b.prototype, proto);
+            proto[j] = b;//give a reference to the inherited constructor
+        }
+        
+        //add any members
+        copy(ensure(definition, syntax.m, {}), proto);
+                
+        //check if we have this constructor, if so run as constructor otherwise run as non-constructor!
+        a = function () {
+            b = arguments;
+            if (this[this[syntax.n]]) {
+                return a[syntax.c].apply(this, b);
+            }
+            return a[syntax.f].apply(a, b);
+        };
+        
+        //set up prototype
+        proto[classname] = a;
+        proto[syntax.n] = classname;
+        
+        //set up class info etc
+        a.prototype = proto;//use proper keyword here
+        a[syntax.d] = definition;//
+        a[syntax.c] = ensure(definition, syntax.c, function () {//default constructor will take an init object and copy its contents
+            copy(arguments[0] || {}, this);
+        });
+        a[syntax.f] = ensure(definition, syntax.f, funner());//copy in non-constructor
+        copy(ensure(definition, syntax.s, {}), a);//copy in statics
+        a[syntax.n] = classname;//set classname
+        
+        if (classname) {
+            link(a, scope);//a has a classname defined so link will use that
+        }
+        return a;
+    }
+    
+    define.k = syntax;//the syntax is made available for editing here
+    
+    return define;
+}(this, {//keywords are editable if you don't like them
     d: 'def',       //definition
     p: 'proto',     //for prototypal inheritance
     x: 'base',      //inherit from base classes
@@ -130,4 +131,4 @@ Joop.k = {//keywords are editable if you don't like them
     c: 'init',      //function to be called when constructing
     f: 'fun',       //function to be called when not constructing
     n: 'classname'  //is classname a good name for this?
-};
+}));
